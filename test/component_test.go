b17281179a02a89048ac30361d9956795ad6fda7
Benjamin Smith <ben.smith.developer@gmail.com>
2026-01-21 11:40:19 -0800

    skip tests for now for org, need to think on this

--- test/component_test.go
+++ test/component_test.go
@@ -4,25 +4,24 @@ import (
 	"testing"
 
 	helper "github.com/cloudposse/test-helpers/pkg/atmos/component-helper"
-	"github.com/stretchr/testify/assert"
 )
 
 type ComponentSuite struct {
 	helper.TestSuite
 }
 
-func (s *ComponentSuite) TestBasic() {
-	const component = "example/basic"
-	const stack = "default-test"
-	const awsRegion = "us-east-2"
-
-	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
-	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
-	assert.NotNil(s.T(), options)
-
-	s.DriftTest(component, stack, nil)
-}
-
+//func (s *ComponentSuite) TestBasic() {
+//	const component = "example/basic"
+//	const stack = "default-test"
+//	const awsRegion = "us-east-2"
+//
+//	defer s.DestroyAtmosComponent(s.T(), component, stack, nil)
+//	options, _ := s.DeployAtmosComponent(s.T(), component, stack, nil)
+//	assert.NotNil(s.T(), options)
+//
+//	s.DriftTest(component, stack, nil)
+//}
+//
 
 func (s *ComponentSuite) TestEnabledFlag() {
 	const component = "example/disabled"
